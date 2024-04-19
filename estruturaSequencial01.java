0d1434a6ff1f783fa8f5170883e85624dc3aae17
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2024-04-18 22:45:35 -0300

    att

---
+++ estruturaSequencial01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class estruturaSequencial01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double largura, comprimento, metroQuadrado, area, preco;
+
+        System.out.println("Digite a lagura: ");
+        largura = scanner.nextDouble();
+        System.out.println("Digite o comprimento: ");
+        comprimento = scanner.nextDouble();
+        System.out.println("Digite o metro quadrado: ");
+        metroQuadrado = scanner.nextDouble();
+
+        area = largura * comprimento;
+        preco = area * metroQuadrado;
+
+
+        System.out.printf("A area é: %.2f%n", area);
+        System.out.printf("O preço por metro quadrado é: %.2f%n", preco);
+
+        scanner.close();
+
+        
+    }
+    
+}
---
+++ estruturaSequencial02.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class estruturaSequencial02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double base, altura, area, perimetro, diagnonal;
+
+        System.out.println("Digite o valor da base: ");
+        base = scanner.nextDouble();
+        System.out.println("Digite a altura: ");
+        altura = scanner.nextDouble();
+        
+        area = base * altura;
+        perimetro = 2.0 * (base + altura);
+        diagnonal = Math.sqrt(Math.pow(base, 2.0) + Math.pow(altura, 2.0));
+
+        System.out.printf("Area: %.4f%n", area);
+        System.out.printf("Perímetro: %.4f%n", perimetro);
+        System.out.printf("Diagonal: %.4f%n", diagnonal);
+
+        scanner.close();
+
+    }
+    
+}
